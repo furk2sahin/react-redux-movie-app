e9ebed062f9fea2541cb0069375d235a1aef562a
furk2sahin <furkannsahin.7@gmail.com>
2020-11-07 04:21:27 +0300

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import MoviesPage from './components/pages/MoviesPage';
+
+import { Link, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Link to="movies">Movies</Link>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Route path='/movies' component={MoviesPage}></Route>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -9,17 +9,20 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
