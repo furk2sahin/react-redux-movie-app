915d35bf76fb332e49ff87a0ed22892add1d14df
furk2sahin <furkannsahin.7@gmail.com>
2020-11-07 04:11:39 +0300

    Add thunk middleware & redux dev tools & Provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import thunk from 'redux-thunk'
+
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/rootReducer'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux'
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
